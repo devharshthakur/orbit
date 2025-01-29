40d73e6e953696b245773875ec64ae3ea1285ece
Harsh Thakur <harshprojects2002@outlook.com>
2025-01-30 00:34:02 +0530

    change/web : : Lint config
    Disable some liniting rules for web folder

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -13,7 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      'react/no-unescaped-entities': 'off'
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-var': 'off'
     }
   }
 ];
